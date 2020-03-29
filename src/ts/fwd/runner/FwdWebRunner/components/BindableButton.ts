e819be7e37bd4927cca3cb9d510189327da6135d
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2020-03-29 16:26:36 +0200

    Add ControlBindingManager and allow multiple binding

--- src/ts/fwd/runner/FwdWebRunner/components/BindableButton.ts
+++ src/ts/fwd/runner/FwdWebRunner/components/BindableButton.ts
@@ -1,6 +1,106 @@
 import { injectStyle } from '../StyleInjector';
+import debounce from '../../../utils/debounce';
+import { Popover } from './Popover';
 
-export class BindableButton {
+export interface BindableControl {
+  controlElement: HTMLElement;
+
+  triggerKeyAction(sourceBinding: KeyBinding): void;
+  setKeyBindingMode(bindingMode: boolean): void;
+  setKeyBindings(bindings: KeyBinding[]): void;
+}
+
+export interface KeyBinding {
+  code: string,
+  control: BindableControl;
+}
+
+export class ControlBindingManager {
+  private _keyBindings: KeyBinding[] = [];
+
+  private _controlBeingEdited: BindableControl = null;
+
+  private readonly _popover: Popover;
+
+  private textToShowIfNoMapping = '(none)';
+
+  constructor() {
+    document.addEventListener('keydown', (evt) => this.handleKeyDown(evt.code));
+    this._popover = new Popover();
+    this._popover.setInnerHTML(this.textToShowIfNoMapping);
+  }
+
+  setControlBeingEdited(control: BindableControl) {
+    if (this._controlBeingEdited !== null) {
+      this._controlBeingEdited.setKeyBindingMode(false);
+    }
+
+    this._controlBeingEdited = control;
+
+    if (this._controlBeingEdited !== null) {
+      const keyBindingsForControl = this._keyBindings.filter(binding => binding.control === control);
+      control.setKeyBindingMode(true);
+      this._popover.sourceElement = control.controlElement;
+      this._popover.setInnerHTML(this.getKeyBindingsAsString(keyBindingsForControl));
+      this._popover.show();
+    } else {
+      this._popover.hide();
+    }
+  }
+
+  private handleKeyDown(code: string) {
+    if (this._controlBeingEdited !== null) {
+      if (code === 'Escape') {
+        this._controlBeingEdited.setKeyBindingMode(false);
+        this._controlBeingEdited = null;
+        return;
+      }
+
+      this.toggleKeyBinding(this._controlBeingEdited, code);
+    } else {
+      this.dispatchKeyEvent(code);
+    }
+  }
+
+  private toggleKeyBinding(control: BindableControl, code: string) {
+    const existingBinding = this.getKeyBinding(control, code);
+
+    if (existingBinding) {
+      this._keyBindings = this._keyBindings.filter((binding) => binding != existingBinding);
+    } else {
+      this._keyBindings.push({ control, code });
+    }
+
+    const keyBindingsForControl = this._keyBindings.filter(binding => binding.control === control);
+    control.setKeyBindings(keyBindingsForControl);
+    this._popover.setInnerHTML(this.getKeyBindingsAsString(keyBindingsForControl));
+  }
+
+  private getKeyBindingsAsString(keyBindings: KeyBinding[]): string {
+    return keyBindings.length === 0 ? this.textToShowIfNoMapping :
+        keyBindings.map(binding => `${binding.code.replace('Key', '')}`)
+        .join(' ');
+  }
+
+  private getKeyBinding(control: BindableControl, code: string): KeyBinding {
+    const results = this._keyBindings.filter((binding) => {
+      return binding.code === code && binding.control === control;
+    });
+
+    return results.length > 0 ? results[0] : null;
+  }
+
+  private dispatchKeyEvent(code: string): void {
+    const bindings = this._keyBindings.filter((binding) => binding.code === code);
+    bindings.forEach((binding) => {
+      binding.control.triggerKeyAction(binding);
+    });
+  }
+}
+
+const controlBindingManager = new ControlBindingManager();
+
+export class BindableButton implements BindableControl {
   public readonly htmlElement: HTMLElement;
 
   public action: Function;
@@ -8,6 +108,8 @@ export class BindableButton {
   private _button: HTMLButtonElement;
 
   private _indicator: HTMLSpanElement;
+
+  private _releaseBlinkDebounced: Function;
 
   constructor(label: string) {
     this.htmlElement = this._button = document.createElement('button');
@@ -20,17 +122,71 @@ export class BindableButton {
       }
     }
 
+    this._button.addEventListener('contextmenu', (evt: MouseEvent) => {
+      evt.preventDefault();
+      evt.stopPropagation();
+      evt.cancelBubble = true;
+      return false;
+    });
+
+    this._button.oncontextmenu = (evt: MouseEvent) => {
+      controlBindingManager.setControlBeingEdited(this);
+    }
+
+    this._button.onblur = (evt: MouseEvent) => {
+      controlBindingManager.setControlBeingEdited(null);
+    }
+
     this._indicator = document.createElement('span');
     this._indicator.classList.add('indicator');
-    // this._indicator.textContent = 'H';
     this._button.append(this._indicator);
   }
+
+  public get controlElement(): HTMLElement { return this.htmlElement; }
 
   public get text(): string { return this._button.innerText; }
   public set text(newLabel: string) { this._button.innerText = newLabel; }
 
   public get disabled(): boolean { return this._button.disabled; }
   public set disabled(disabled: boolean) { this._button.disabled = disabled; }
+  
+  public triggerKeyAction(sourceBinding: KeyBinding) {
+    this.blink();
+    this.action();
+  }
+
+  setKeyBindings(bindings: KeyBinding[]) {
+    if (bindings === null || bindings.length === 0) {
+      this._indicator.classList.remove('bound');
+      return;
+    }
+
+    this._indicator.classList.add('bound');
+
+  }
+
+  setKeyBindingMode(bindingMode: boolean) {
+    if (bindingMode) {
+      this._button.classList.add('binding');
+    } else {
+      this._button.classList.remove('binding');
+    }
+  }
+
+  private blink() {
+    const releaseTime = 300;
+    const cssClass = 'blinking';
+
+    this._indicator.classList.add(cssClass);
+
+    if (this._releaseBlinkDebounced == null) {
+      this._releaseBlinkDebounced = debounce(() => {
+        this._indicator.classList.remove(cssClass);
+      }, releaseTime);
+    }
+
+    this._releaseBlinkDebounced();
+  }
 }
 
 injectStyle(BindableButton.name, `
@@ -42,8 +198,23 @@ injectStyle(BindableButton.name, `
     margin: auto 0 auto 7px;
   }
 
+  .indicator.bound.blinking {
+    background: #00b7ff;
+  }
+
+  .indicator.bound {
+    background: #69b2cfa1;
+  }
+
   .bindable-button {
     display: flex;
     padding-right: 7px;
+    margin: 1px 3px;
+  }
+
+  .bindable-button.binding {
+    border: 2px solid #69b2cf;
+    background: #69b2cf61;
+    margin: 0 2px;
   }
 `);
---
+++ src/ts/fwd/runner/FwdWebRunner/components/Popover.ts
@@ -0,0 +1,146 @@
+import { injectStyle } from '../StyleInjector';
+
+function isInViewport(element: HTMLElement) {
+  const rect = element.getBoundingClientRect();
+  const html = document.documentElement;
+  return rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth);
+}
+
+type PopoverPosition = 'top' | 'left' | 'right' | 'bottom';
+
+export class Popover {
+  private readonly popover: HTMLDivElement;
+
+  private readonly className = 'popover';
+
+  constructor(private _sourceElement?: HTMLElement, 
+      public readonly position: PopoverPosition = 'top') {
+    this.popover = document.createElement('div');
+    this.popover.style.position = 'fixed';
+    this.popover.classList.add(this.className);
+  }
+
+  get sourceElement(): HTMLElement { return this._sourceElement; }
+
+  set sourceElement(source: HTMLElement) {
+    this._sourceElement = source;
+
+    if (this.isVisible) {
+      this.show();
+    }
+  }
+
+  get isVisible() {
+    return !!document.body && document.body.contains(this.popover);
+  }
+
+  show() {
+    if (this.sourceElement == null) {
+      return;
+    }
+
+    document.addEventListener('click', this.handleDocumentEvent);
+    window.addEventListener('scroll', this.handleWindowEvent);
+    window.addEventListener('resize', this.handleWindowEvent);
+
+    document.body.appendChild(this.popover);
+
+    const { top: triggerTop, left: triggerLeft } = this.sourceElement.getBoundingClientRect();
+    const { offsetHeight: triggerHeight, offsetWidth: triggerWidth } = this.sourceElement;
+    const { offsetHeight: popoverHeight, offsetWidth: popoverWidth } = this.popover;
+
+    const orderedPositions = ['top', 'right', 'bottom', 'left'];
+    const positionIndex = orderedPositions.indexOf(this.position);
+
+    const positions = {
+      top: {
+        name: 'top',
+        top: triggerTop - popoverHeight,
+        left: triggerLeft - ((popoverWidth - triggerWidth) / 2)
+      },
+      right: {
+        name: 'right',
+        top: triggerTop - ((popoverHeight - triggerHeight) / 2),
+        left: triggerLeft + triggerWidth
+      },
+      bottom: {
+        name: 'bottom',
+        top: triggerTop + triggerHeight,
+        left: triggerLeft - ((popoverWidth - triggerWidth) / 2)
+      },
+      left: {
+        name: 'left',
+        top: triggerTop - ((popoverHeight - triggerHeight) / 2),
+        left: triggerLeft - popoverWidth
+      }
+    };
+
+    const position = orderedPositions
+      .slice(positionIndex)
+      .concat(orderedPositions.slice(0, positionIndex))
+      .map(pos => positions[pos])
+      .find(pos => {
+        this.popover.style.top = `${pos.top}px`;
+        this.popover.style.left = `${pos.left}px`;
+        return isInViewport(this.popover);
+      });
+
+    if (! position) {
+      this.popover.style.top = positions.bottom.top + 'px';
+      this.popover.style.left = positions.bottom.left + 'px';
+    }
+  }
+
+  hide() {
+    this.popover.remove();
+
+    document.removeEventListener('click', this.handleDocumentEvent);
+    window.removeEventListener('scroll', this.handleWindowEvent);
+    window.removeEventListener('resize', this.handleWindowEvent);
+  }
+
+  toggle() {
+    if (this.isVisible) {
+      this.hide();
+    } else {
+      this.show();
+    }
+  }
+
+  setInnerHTML(html: string) {
+    this.popover.innerHTML = html;
+
+    if (this.isVisible) {
+      this.show();
+    }
+  }
+
+  private handleWindowEvent() {
+    if (this.isVisible) {
+      this.show();
+    }
+  };
+
+  private handleDocumentEvent(evt: MouseEvent) {
+    if (this.isVisible && evt.target !== this.sourceElement && evt.target !== this.popover) {
+      this.hide();
+    }
+  }
+}
+
+injectStyle('Popover', `
+.popover-container {
+  margin: 300px;
+}
+
+.popover {
+  padding: 8px;
+  border: 1px solid $border-color;
+  border-radius: 4px;
+  background: white;
+  box-shadow: 0 1px 4px rgba(0,0,0,.2);
+}
+`);
