e0026648296d41451dc6915d22e09dd2b47a2e18
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2020-05-02 01:01:43 +0200

    Defer AudioParam automation when calling rampTo and keep internal state for the latest ramp
    Deferring the time ramp seem to provide a more consistent behaviour across browsers and with large lookAhead values. It also removes the necessity of calling the unclearly speced cancelAndHoldAtTime by providing a custom implementation. This is possible because we're in a subset situation of the WebAudio spec where automation event should always be scheduled in a chronological order.

--- src/fwd/audio/nodes/StandardAudioNodes.ts
+++ src/fwd/audio/nodes/StandardAudioNodes.ts
@@ -2,15 +2,24 @@ import path from "path";
 import { Time } from "../../core/EventQueue/EventQueue";
 import { fwd } from "../../core/Fwd";
 import { clamp } from "../../core/utils/numbers";
-import { Logger, LoggerLevel } from "../../utils/dbg";
+// import { Logger, LoggerLevel } from "../../utils/dbg";
 import { FwdAudio } from "../FwdAudio";
-import parentLogger from "../logger.audio";
+// import parentLogger from "../logger.audio";
 import { FwdAudioNode } from "./FwdAudioNode";
 
-const DBG = new Logger('StandardAudioNodes', parentLogger, LoggerLevel.debug);
+// const DBG = new Logger('StandardAudioNodes', parentLogger, LoggerLevel.debug);
+
+interface LinearRamp {
+  startTime: number,
+  startValue: number,
+  endTime: number,
+  endValue: number,
+}
 
 export class FwdAudioParamWrapper extends FwdAudioNode {
   public outputNode: AudioNode = null;
+
+  private _latestRamp: LinearRamp;
 
   constructor(readonly fwdAudio: FwdAudio, private _param: AudioParam) {
     super();
@@ -20,30 +29,77 @@ export class FwdAudioParamWrapper extends FwdAudioNode {
     return this._param;
   }
 
+  /**
+   * Sets the value at the current FwdScheduler's time position.
+   * @param newValue the value the param should take. This value is not checked against the validity range of the underlying
+   * AudioParam.
+   */
   public set value(newValue: number) {
-    this._param.value = newValue;
+    // Still process the event as a ramp to have a consistent 'getValueAtTime' behaviour.
+    this.rampTo(newValue, 0);
   }
 
+  /**
+   * Gets the value at the current audio context time.
+   */
+  // TODO: should we take into account the currently scheduled ramps like in 'getValueAtTime' ?
   public get value(): number { return this._param.value}
 
-  public rampTo(value: number, time: number): void {
-    const audioNow = this.fwdAudio.now();
+  /**
+   * Starts a ramp from the FWD scheduler's time position to a time position in the future (current time + rampTime).
+   * This is a deferred action: the automation will be effectively scheduled 'later', based on the FwdScheduler's settings.
+   *
+   * @param value the target value
+   * @param rampTime the duration for the ramp
+   */
+  public rampTo(value: number, rampTime: number): void {
+    fwd.schedule(0, () => {
+      const audioNow = this.fwdAudio.now();
+      const holdValue = this.cancelAndHoldAtTime(audioNow);
+      this._param.linearRampToValueAtTime(value, audioNow + rampTime);
 
-    // Check if cancelAndHoldAtTime is implemented
-    if (typeof this._param.cancelAndHoldAtTime === 'function') {
-      this._param.cancelAndHoldAtTime(audioNow);
-      this._param.linearRampToValueAtTime(value, audioNow + time);
-    } else {
-      // Falls back to deferring the call to linearRampToValueAtTime
-      fwd.schedule(0, () => {
-        this._param.setValueAtTime(this._param.value, 0);
-        this._param.linearRampToValueAtTime(value, audioNow + time);
-      });
-    }
+      this.registerRamp(holdValue, audioNow, value, audioNow + rampTime);
+    }, true);
   }
 
   protected doTearDown(/* when: Time */): void {
     throw new Error('You shouldn\'t call "tearDown" on a audio parameter.');
+  }
+
+  private cancelAndHoldAtTime(when: Time): number {
+    const holdValue = this.getValueAtTime(when);
+    // DBG.debug('holdValue : ' + holdValue);
+    this._param.setValueAtTime(holdValue, when);
+    return holdValue;
+  }
+
+  private registerRamp(startValue: number, startTime: Time, endValue: number, endTime: Time): void {
+    this._latestRamp = {
+      startTime, startValue, endTime, endValue,
+    };
+  }
+
+  private getValueAtTime(when: Time): number {
+    // If no ramp was ever scheduled, just return the current native AudioParam's value.
+    if (this._latestRamp == null) {
+      // DBG.debug('no ramp');
+      return this._param.value;
+    }
+
+    if (when < this._latestRamp.startTime) {
+      // DBG.debug('before');
+      return this._latestRamp.startValue;
+    } else if (when > this._latestRamp.endTime) {
+      // DBG.debug('after');
+      return this._latestRamp.endValue;
+    } else {
+      // Linear interpolation
+      const t1 = this._latestRamp.startTime;
+      const t2 = this._latestRamp.endTime;
+      const v1 = this._latestRamp.startValue;
+      const v2 = this._latestRamp.endValue;
+      return ((v1 - v2) / (t1 - t2)) * when + ((t1 * v2 - t2 * v1) / (t1 - t2));
+    }
   }
 }
 
@@ -104,6 +160,7 @@ export class FwdOscillatorNode extends FwdAudioNode {
   public static MAX_FREQ: number = 40000;
 
   public readonly gain: FwdAudioParamWrapper;
+  public readonly frequency: FwdAudioParamWrapper;
   
   private _output: GainNode;
   private _osc: OscillatorNode;
@@ -131,6 +188,7 @@ export class FwdOscillatorNode extends FwdAudioNode {
     this._osc.start(fwdAudio.now());
 
     this.gain = new FwdAudioParamWrapper(this.fwdAudio, this._output.gain);
+    this.frequency = new FwdAudioParamWrapper(this.fwdAudio, this._osc.frequency);
   }
 
   public get inputNode(): AudioNode | AudioParam { return null; }
@@ -138,11 +196,6 @@ export class FwdOscillatorNode extends FwdAudioNode {
 
   public get oscillator(): OscillatorNode {
     return this._osc;
-  }
-
-  public get frequency(): FwdAudioParamWrapper {
-    this.assertIsReady('get frequency');
-    return new FwdAudioParamWrapper(this.fwdAudio, this._osc.frequency);
   }
 
   public get type(): OscillatorType {
