1815089964899350f38f5f4f0c737eb47c6dd78f
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2020-05-16 11:05:40 +0200

    Re-indenting

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,29 @@ import FwdRunner from './fwd/runner/FwdRunner';
 import FwdWebRunner from './fwd/runner/FwdWebRunner/FwdWebRunner';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const runner: FwdRunner = new FwdWebRunner();
+  const runner: FwdRunner = new FwdWebRunner();
 
-    runner.entryPoint = () => {
-        const module = hardLoadModule();
-        runner.sketchModule = module;
+  runner.entryPoint = () => {
+    const module = hardLoadModule();
+    runner.sketchModule = module;
 
-        if (typeof module['init'] === 'function') {
-            module.init();
-        } else {
-            runner.logger.err(null, `It seems like your sketch doesn't export a 'init' function.`);
-        }
-    };
+    if (typeof module['init'] === 'function') {
+      module.init();
+    } else {
+      runner.logger.err(null, `It seems like your sketch doesn't export a 'init' function.`);
+    }
+  };
 
-    const m = require('./sketch');
-    runner.sketchModule = require('./sketch');
+  const m = require('./sketch');
+  runner.sketchModule = require('./sketch');
 
-    runner.actions = Object.keys(m)
-        .filter((e) => typeof m[e] === 'function' 
-                    && e !== 'init' 
-                    && m[e].length === 0);
+  runner.actions = Object.keys(m)
+    .filter((e) => typeof m[e] === 'function'
+      && e !== 'init'
+      && m[e].length === 0);
 });
 
 function hardLoadModule(): any {
-    delete require.cache[require.resolve('./sketch')];
-    return module = require('./sketch') as any;
+  delete require.cache[require.resolve('./sketch')];
+  return module = require('./sketch') as any;
 }
