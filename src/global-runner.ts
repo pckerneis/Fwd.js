4d894748c74a7c9e3e152fbec4c2ebd081414c5d
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2020-05-16 10:26:16 +0200

    Add basic WebSocket hot reload

--- src/global-runner.ts
+++ src/global-runner.ts
@@ -1,14 +1,37 @@
 import { fwd } from "./fwd/core/Fwd";
+import rootLogger from './fwd/logger.fwd';
 import FwdRunner from './fwd/runner/FwdRunner';
 import FwdWebRunner from './fwd/runner/FwdWebRunner/FwdWebRunner';
 import { Logger, LoggerLevel } from "./fwd/utils/dbg";
 
 Logger.runtimeLevel = LoggerLevel.error;
 
+const DBG = new Logger('global-runner', rootLogger);
+
+let sketchModule: any;
+
+// TODO: action buttons are broken for now
+
+function executeSketchCode(str: any): void {
+    const firstTimeReceived = sketchModule == null;
+
+    sketchModule = Function(str);
+
+    if (firstTimeReceived) {
+        sketchModule();
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const runner: FwdRunner = new FwdWebRunner();
 
     runner.entryPoint = () => {
+        if (sketchModule == null) {
+            runner.logger.err(null, `The sketch file is not ready or could not be found.`);
+        }
+
+        sketchModule();
+
         if (typeof (window as any).init === 'function') {
             (window as any).init();
         } else {
@@ -18,18 +41,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
     (window as any).fwd = fwd;
 
-    if (typeof (window as any).init === 'function') {
-        (window as any).init();
-    }
+    const ws = new WebSocket(location.origin.replace(/^http/, 'ws'));
+    const handshakeMessage = '__HANDSHAKE__';
 
-    // TODO: action buttons are broken for now
-    /*
-    const m = require('./sketch');
-    runner.sketchModule = require('./sketch');
+    ws.onmessage = msg => {
+        DBG.debug('Message received from server.');
 
-    runner.actions = Object.keys(m)
-        .filter((e) => typeof m[e] === 'function' 
-                    && e !== 'init' 
-                    && m[e].length === 0);
-     */
+        try {
+            const { type, textContent } = JSON.parse(msg.data);
+
+            if (type === 'sketch') {
+                DBG.debug('Sketch received.');
+                executeSketchCode(textContent);
+            } else if (type === 'cssInject') {
+                DBG.debug('Stylesheet received.');
+                Array.from(document.querySelectorAll('link'))
+                 // .filter(link => link.href.includes(path))
+                  .forEach(link => {
+                      link.href = link.href;
+                      DBG.debug('Refresh style sheet', link.href);
+                  });
+            }
+        } catch (e) {
+            DBG.error('Unhandled message received from server.');
+        }
+    };
+
+    ws.onopen = () => {
+        ws.send(handshakeMessage);
+    };
+
+    // Wait for gesture to start audio
+    const firstGestureListener = () => {
+        DBG.info('Starting audio context.');
+        document.removeEventListener("mousedown", firstGestureListener);
+    };
+
+    document.addEventListener("mousedown", firstGestureListener);
 });
+
